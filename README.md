8587d3b4e98685f4fb273b1a3756a9014af1810c
howardchn <howardch@outlook.com>
2019-01-04 13:48:25 +0800

    add readme and fixed some bugs

---
+++ README.md
@@ -0,0 +1,11 @@
+Note: Require python 3.6 installed; doesn't work fine for 3.7
+
+```
+source bin/activate
+pip install -r requirements.txt
+python main.py
+```
+
+![image 1](https://github.com/howardchn/prophet-demo/raw/master/out/https-response-time-month.png)
+
+![image 2](https://github.com/howardchn/prophet-demo/raw/master/out/relayserver-cpu-month.png)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from fbprophet import Prophet
 import matplotlib.pyplot as plt
-
+from stopwatch import Stopwatch
 
 def load_data(path, column_mappings = {'Time': 'ds', 'prod08.ld5': 'y'}):
     data = pd.read_csv(path)
@@ -12,7 +12,7 @@ def load_data(path, column_mappings = {'Time': 'ds', 'prod08.ld5': 'y'}):
 
 def fix_nan_data(df):
     for index, row in df.iterrows():
-        if row['y'].strip() == 'No Data':
+        if isinstance(row['y'], str) and row['y'].strip() == 'No Data':
             row.loc['y'] = None
             df.loc[index] = row
 
@@ -20,24 +20,27 @@ def run():
     # df = load_data('./data/example_wp_log_peyton_manning.csv')
     # df = load_data('./data/santaba-demo4.csv')
     # df = load_data('./data/relayserver-CPU-1-month.csv', { 'Time': 'ds', 'cpu_usagePercentCores': 'y' })
-    df = load_data('./data/relayserver-CPU-3-month.csv', { 'Time': 'ds', 'cpu_usagePercentCores': 'y' })
-    # df = load_data('./data/HTTPS-Response_Time.csv', { 'Time': 'ds', 'Page load time': 'y' })
+    # df = load_data('./data/relayserver-CPU-3-month.csv', { 'Time': 'ds', 'cpu_usagePercentCores': 'y' })
+    df = load_data('./data/HTTPS-Response_Time.csv', { 'Time': 'ds', 'Page load time': 'y' })
     
     fix_nan_data(df)
 
-    df.head()
+    print('prophet starting...')
+    sw = Stopwatch()
+    sw.start()
 
     m = Prophet()
     m.fit(df)
 
     future = m.make_future_dataframe(periods = 0)
-    future.tail()
     
     forecast = m.predict(future)
-    forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+    forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+
+    sw.stop()
+    print('prophet complete. training and predict take {}s'.format(round(sw.duration, 3)))
 
     fig1 = m.plot(forecast)
-    plt.title('Relayserver CPU 1 Month', verticalalignment='baseline')
 
     for i in range(len(forecast.values)):
         p = forecast.values[i]
